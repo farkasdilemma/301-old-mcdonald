a68887c925f5dd01dbe3b2e8ff34bfa54b8ce788
farkasdilemma <62410633+farkasdilemma@users.noreply.github.com>
2022-03-21 23:51:42 -0700

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ list_of_exports = ['total exports', 'beef', 'pork', 'poultry',
        'veggies proc', 'total veggies', 'corn', 'wheat', 'cotton']
 
 mycolumn='corn'
-myheading1 = f"Wow! That's a lot of {mycolumn}!"
+#myheading1 = f"Wow! That's a lot of {mycolumn}!"
 mygraphtitle = '2011 US Agriculture Exports by State'
 mycolorscale = 'ylorrd' # Note: The error message will list possible color scales.
 mycolorbartitle = "Millions USD"
@@ -30,20 +30,23 @@ githublink = 'https://github.com/austinlasseter/dash-map-usa-agriculture'
 import pandas as pd
 df = pd.read_csv('assets/usa-2011-agriculture.csv')
 
-fig = go.Figure(data=go.Choropleth(
-    locations=df['code'], # Spatial coordinates
-    z = df[mycolumn].astype(float), # Data to be color-coded
-    locationmode = 'USA-states', # set of locations match entries in `locations`
-    colorscale = mycolorscale,
-    colorbar_title = mycolorbartitle,
-))
+def update_graph(myColumn):
+       fig = go.Figure(data=go.Choropleth(
+              myheading1 = f"Wow! That's a lot of {myColumn}!"
+              locations=df['code'], # Spatial coordinates
+              z = df[myColumn].astype(float), # Data to be color-coded
+              locationmode = 'USA-states', # set of locations match entries in `locations`
+              colorscale = mycolorscale,
+              colorbar_title = mycolorbartitle,
+              ))
 
-fig.update_layout(
-    title_text = mygraphtitle,
-    geo_scope='usa',
-    width=1200,
-    height=800
-)
+       fig.update_layout(
+              title_text = mygraphtitle,
+              geo_scope='usa',
+              width=1200,
+              height=800
+              )
+       return fig
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -74,6 +77,8 @@ app.layout = html.Div(children=[
 @app.callback(Output('figure-1', 'figure'),
        Input('my-dropdown','value')
        )
+def update_graph_callback(value):
+       return update_graph(value)
 
 ############ Deploy
 if __name__ == '__main__':
